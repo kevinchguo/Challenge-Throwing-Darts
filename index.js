b3b48a9ec0664446eee9e0d668ad7226abdd82aa
Kevin Guo <Kevin@Kevins-Air.home>
2019-08-21 19:35:58 -1000

    finished

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 function scoreCalculator(arr) {
   // do work here
+  if (arr.every((elem) => { return elem < 21 })) {
+    if (arr.length === 0) {
+      return -1;
+    } else if (arr.every((elem) => { return elem < 5 })) {
+      return (arr.length * 10) + 100;
+    } else if (arr.every((elem) => { return elem > 10 })) {
+      return 0;
+    } else {
+      let totalScore = 0;
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 5) {
+          totalScore += 10;
+        } else if (arr[i] >= 5 && arr[i] < 11) {
+          totalScore += 5;
+        }
+      }
+      return totalScore;
+    }
+  }
+  return 0;
 }
-
 module.exports = scoreCalculator;
